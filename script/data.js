db415e04f68e8e0ce78efd05875a5dbf4544db43
Pavel Uzunyants <hedeeras@yandex.ru>
2022-11-18 23:15:24 +0300

    features/ Сделал массив с карточками, рендер карточек происходит через скрипт, филтрация на мужское и женское

--- script/data.js
+++ script/data.js
@@ -1,29 +1,29 @@
 const cardsMassiveWoman = [
   {
-    image: './images/Slider_girls/жен худи 2.jpg',
-    title: 'Худи в сорочку',
-    description: 'Каштановый в кашку',
-    price: '3 200 руб',
+    image: './images/Slider_girls/жен худи 1.jpg',
+    title: 'Костюм с худи',
+    description: 'Худи и штаны в цвете «капучино», базовая белая футболка',
+    price: '7 000 руб',
   },
   {
-    image: './images/Slider_girls/жен худи 1.jpg',
-    title: 'Худи в сорочку',
-    description: 'Каштановый в кашку',
-    price: '3 200 руб',
+    image: './images/Slider_girls/жен свитшот 1.jpg',
+    title: 'Костюм со свитшотом',
+    description: 'Свитшот и штаны в цвете «капучино», базовая белая футболка',
+    price: '6 500 руб',
   },
 ]
 
 const cardsMassiveMen = [
   {
-    image: './images/With_hoodi.jpg',
-    title: 'Худи в сорочку',
-    description: 'Каштановый в оранжевом',
-    price: '5 200 руб',
+    image: './images/Slider_boys/муж худи 1.jpg',
+    title: 'Костюм с худи',
+    description: 'Худи и штаны в цвете «капучино», базовая белая футболка',
+    price: '3 200 руб',
   },
   {
-    image: './images/With_hoodi.jpg',
-    title: 'Худи в сорочку',
-    description: 'Каштановый в черном',
-    price: '9 200 руб',
-  },
+    image: './images/Slider_boys/муж свитшот 1.jpg',
+    title: 'Костюм со свитшотом',
+    description: 'Свитшот и штаны в цвете «капучино», базовая белая футболка',
+    price: '4 200 руб',
+  }
 ]
--- script/slider.js
+++ script/slider.js
@@ -1,6 +1,10 @@
 const cardTemplate = document.querySelector('#card-template').content.querySelector('.choice-suit__card')
 const buttonChangeSlider = document.querySelector('.choice-suit__slider-treck-button')
 const cardContainer = document.querySelector('.choice-suit__card-container')
+const buttonGenW = document.querySelector('#genW')
+const buttonGenM = document.querySelector('#genM')
+
+let current = 0
 
 
 // Создание новой карточки
@@ -18,15 +22,47 @@ const createSliderCard = (item) => {
   const cardPrice = cardNew.querySelector('.choice-suit__slider-price')
   cardPrice.textContent = item.price
 
+  // const buttonTreck = cardNew.querySelector('#treck1')
+  // // buttonTreck.addEventListener('click',)
+
+  buttonGenM.addEventListener('click', () => {
+    cardNew.remove()
+  })
+
+  buttonGenW.addEventListener('click', () => {
+    cardNew.remove()
+  })
+
   return cardNew
 }
 
+
 // Рендерим
 const renderCard = (item) => {
-  cardContainer.prepend(createSliderCard(item))
+  cardContainer.append(createSliderCard(item))
 }
 
+
 // Прогоняем по массиву
-cardsMassiveWoman.forEach((item) => {
+cardsMassiveWoman.slice(0, 7).map((item) => {
   renderCard(item)
+  return
+})
+
+buttonGenM.addEventListener('click', () => {
+  buttonGenW.classList.remove('choice-suit__gender-bar-button_active')
+  buttonGenM.classList.add('choice-suit__gender-bar-button_active')
+  cardsMassiveMen.slice(0, 2).map((item) => {
+    renderCard(item)
+    return
+  })
+})
+
+buttonGenW.addEventListener('click', () => {
+  buttonGenM.classList.remove('choice-suit__gender-bar-button_active')
+  buttonGenW.classList.add('choice-suit__gender-bar-button_active')
+  cardsMassiveWoman.slice(0, 2).map((item) => {
+    renderCard(item)
+    return
+  })
 })
